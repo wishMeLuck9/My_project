c417f377318c9ba4d8bd285383c718a6c29ed21d
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-03-12 23:46:54 +0000

    camera

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    public Vector3 offset = new Vector3(64, 0, 50);
+    private Vector3 offset = new Vector3(40, 0, 0);
 
     // Start is called before the first frame update
     void Start()
